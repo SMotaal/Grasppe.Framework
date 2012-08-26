25c7bb1c0beceda1b61878231a896837402fab4d
daflair <daflair@mac.com>
2012-08-26 11:54:24 -0400

    MatLab grasppeScreen3: subs refactored, curve-lookup feature implemented

--- Grasppe.MatLab/Common/grasppeScreen3.m
+++ Grasppe.MatLab/Common/grasppeScreen3.m
@@ -2,20 +2,21 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
   %HALFTONE1 Summary of this function goes here
   %   Detailed explanation goes here
   
-  persistent screenGrid screenFrequency screenSize screens;
+  %persistent screenGrid screenFrequency screenSize screens;
   
   import Grasppe.ConRes.PatchGenerator.Parameters.*;
   import Grasppe.ConRes.PatchGenerator.*;
   
-  bufferScreen  = false;
-  bufferGrid    = true;
-  bufferPersist = false; % bufferScreen;
+  % bufferScreen  = false;
+  % bufferGrid    = true;
+  % bufferPersist = false; % bufferScreen;
+  %
+  % rotateScreen  = true;
+  % rotateCells   = false;
+  % rotateImage   = false;
   
-  rotateScreen  = true;
-  rotateCells   = false;
-  rotateImage   = false;
-  
-  outputScreen  = false;
+  outputScreen    = false; %true;
+  outputRaster    = false; %true;
     
   %% Screening Settings
   DEFAULT = {2450, 175, 37.5};      % {2400, 150};  
@@ -29,7 +30,7 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
   end
   
   if ~exist('lpi', 'var') || ~isscalar(ppi) || ~isnumeric(ppi)
-    spi = DEFAULT{2};
+    lpi = DEFAULT{2};
   end  
   
   if ~exist('angle', 'var') || ~isscalar(ppi) || ~isnumeric(ppi)
@@ -52,20 +53,18 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
       BS  = findField(printing, 'Radius');
   end
   
-  % if BS > 0, BP = 1; end
+  if BP==0 || BS==0
+    BP = 0;
+    BS = 0;
+  end
 
   SPI     = spi;       % Spots/Inch  [Screening Addresibility    ]
   LPI     = lpi;       % Lines/Inch  [Screening Resolution       ]
   
-  DPI     = 2*LPI;            % Dots/Inch   [Raster Stream Resolution   ]
-  
-  SPL     = SPI/LPI;          % Spots/Line  [Screening Ruling           ]
-  ANGLE   = 45+angle; %[15 75 0 45];   % Degrees     [Screening Angle            ]
-  
   %% Load Image
   
   if exist('imagePath', 'var') && (isnumeric(imagePath) || islogical(imagePath))
-    cmykData = imagePath;
+    contone = imagePath;
     
     imagePath = inputname(1);
     
@@ -80,18 +79,14 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
     
     [filepath prefix suffix] = fileparts(imagePath);
     
-    % T = tic; %fprintf('Loading Image %s... ', [prefix suffix]);
-    
-    cmykData = imread(imagePath);
-    
-    % try toc(T); end
+    contone = imread(imagePath);
     
   end
+
+  contone = im2double(contone);
   
   PPI     = ppi;           % Pixel/Inch  [Image Resolution           ]
-  PPS     = PPI/SPI;       % Pixel/Spot  [Raster Image Resolution    ]
-  
-  NC      = size(cmykData,3);
+  ANGLE   = angle;
   
   %% Preparing Output Filenaming
   if ~exist('prefix', 'var') || ~ischar(prefix)
@@ -106,7 +101,8 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
   
   try
     outpath  = @(x) fullfile('./Output', outputFolder, x);
-    if ~exist(outpath('')), mkdir(outpath('')); end
+    %if ~exist(outpath('')), 
+    FS.mkDir(outpath(''));
   catch err %if ~exist('outputFolder', 'var') || ~ischar(outputFolder)
     outpath  = @(x) fullfile('./Output/', x);
   end
@@ -118,24 +114,198 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
     imlpi     = LPI;
     imspi     = SPI;
     imangles  = sprintf('%dº ', ANGLE);
-    mstamp    = [mfilename ' (' num2str(mfilerev) ')'];
+    mstamp    = [mfilename ' (' num2str(MX.stackRev) ')'];
     imdisc    = sprintf('%s %1.1f dpi screened using %s at %1.1f lpi / %1.1f spi', ...
       imname, imdpi, mstamp, imlpi, imspi);
   end
+  
+  %[halftone raster screen contone] = screenImage(contone, PPI, SPI, LPI, ANGLE, DP, NP, BP, BS);
+  
+  curve = createToneCurve(SPI, LPI, ANGLE, DP, NP, BP, BS);
+  
+  for m = 1:size(contone,3)
+    curvedtone(:,:,m) = appleToneCurves(contone(:,:,m), curve);
+  end
+  
+  if outputScreen || outputRaster
+    [halftone raster screen] = screenImage(curvedtone, PPI, SPI, LPI, ANGLE, DP, NP, BP, BS);
+  else
+    halftone = screenImage(contone, PPI, SPI, LPI, ANGLE, DP, NP, BP, BS);
+  end
+  
+  FS.mkDir(outpath(''));
+    
+  if outputRaster %numel(raster)>1
+    for m = 1:numel(raster)
+      
+      while exist(outpath(filename),'file') > 0
+        fileno = fileno+1;
+        filename = [prefix int2str(fileno) suffix];
+      end
+      
+      imwrite(raster{m}, outpath(filename), 'Resolution', SPI, 'Description', imdisc);
+      
+    end
+  end
+  
+  if outputScreen
+    for m = 1:numel(screen)
+    
+      imwrite(screen{m}, outpath([prefix int2str(fileno) '.scrn' suffix]), ...
+        'Resolution', SPI, 'Compression', 'lzw', 'Description', imdisc);    
+      
+    end
+  end  
+  
+  Output = halftone;
+  
+  imwrite(halftone, outpath([prefix suffix]), ...
+    'Resolution', SPI, 'Compression', 'lzw', 'Description', imdisc);
+  
+  
+  if nargout > 0
+    try stack.Screen  = Screen;       end
+    try stack.Output  = Output;       end
+  end
+  
+  
+end
 
+
+function curve = createToneCurve(spi, lpi, angle, gain, noise, blur, radius) % screenID)
   
-  clear cmykRaster;
+  screenID    = generateScreenID(spi, lpi, angle, gain, noise, blur, radius);
+  curvePath   = curvesPath(screenID);
   
-  for m=1:NC %:4
-    clear imageData;
+  outputLinear = true;
+  
+  if outputLinear
+    linearPath  = fullfile('.','Output', 'Linear');
+    FS.mkDir(linearPath);
+  end
+  
+  if ~(exist(curvePath, 'file')==2)
+    
+    steps = 101;
+    
+    in    = linspace(0,100, steps);
+    out   = linspace(0,100, steps);
+    
+    %% Create Curves
+    for m = numel(in)
+      contone   = ones(15, 15) .* in(m)/100;
+      halftone  = screenImage(contone, lpi, spi, lpi, angle, gain, noise, blur, radius);
+      meantone  = mean(halftone(:));
+      
+      out(m)    = meantone*100;
+
+      if outputLinear, imwrite(halftone, fullfile(linearPath ,['tv' num2str(in(m),'%03d') '.png'])); end
+    end
+    
+    %curve = [in(:) out(:)];
+    
+    if any(in~=out)
+      curve = [in(:) out(:)];
+    else
+      curve = [0 0; 100 100];
+    end
+    
+    dlmwrite(curvePath, curve);
+    
+  else
+    %% Load Curves
+    
+    curve = dlmread(curvePath);
+  end
+end
+
+function image = appleToneCurves(image, curve)
+  in    = curve(:,1)/100;
+  out   = curve(:,2)/100;
+  
+  if ~any(in~=out), return; end
+  
+  src   = im2double(image);
+  dst  = zeros(size(src));  
+  
+  sig   = sort(unique(src(:)));
+  resp  = interp1(out, in, sig, 'spline');
+  
+  for m     = 1:numel(sig)
+    s       = sig(m);
+    r       = resp(m);
+    n       = src==s;
+    dst(n)  = r;
+  end
+  
+  image = dst;
+end
+
+function pth = curvesPath(screenID)
+  persistent mpth;
+  
+  if isempty(mpth)
+    mxi   = MX.stackInfo;
+    mpth  = mxi.path;
+  end
+  
+  if ~exist('screenID', 'var') || ~ischar(screenID), screenID = ''; end
+  
+  pth = fullfile(mpth, 'data', 'Curves');
+  
+  FS.mkDir(pth);
+  
+  pth = fullfile(pth, [screenID '.csv']);
+end
+
+function id = generateScreenID(spi, lpi, angle, gain, noise, blur, radius)
+  fn  = 'ALTGNBS';
+    
+  rev = MX.stackRev;
+  
+  id  = round(abs([spi lpi mod(angle,90)*10 gain noise blur radius]));
+  id  = dec2hex(id);
+  id  = strcat(fn', id, '.');
+  id  = id';
+  id  = id(:)';
+  
+  id  = regexprep(id, '(?<=[\.\w])0+(?=[^\.])', '');
+  
+  id  = [id 'R' rev];
+  
+  id  = regexprep(id, '\W+', '-');
+end
+
+
+function [halftone raster screen contone] = screenImage(contone, PPI, SPI, LPI, ANGLE, DP, NP, BP, BS)
+  
+  persistent screenGrid screenFrequency screenSize screens;
+  
+  bufferScreen  = false;
+  bufferGrid    = true;
+  bufferPersist = false;
+  
+  rotateScreen  = true;
+  rotateCells   = false;
+  rotateImage   = false;  
+  
+  NC      = size(contone,3);
+  PPS     = PPI/SPI;
+  SPL     = SPI/LPI;
+  DPI     = 2*LPI;
+  ANGLE   = 45+ANGLE;
+  ID      = generateScreenID(SPI, LPI, ANGLE, DP, NP, BP, BS);
+  
+  outputRaster = nargout > 1;
+  outputScreen = nargout > 2;
+  
+  for m=1:NC
     
     %% Prepare Image
+
+    imageData = contone(:,:,m);
     
-    % T = tic; %fprintf('\tPreparing Image... ');
-    imageData = cmykData(:,:,m);
-    
-    mv = 100-im2double(imageData).*100; %[...
-    
+    mv = 100-im2double(imageData).*100;    
     
     [mvw  ] = size(mv,2);
     [mvh  ] = size(mv,1);
@@ -143,17 +313,13 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
     % try toc(T); end
     
     %% Generate Screen
-    % Z = COS(Y/PITCH) * COS(X/PITCH) ) < ((TONE-50) * 0.02)
     
-    mt = ANGLE(m); %for t = (1:90)*pi/180   %pause(0.5);
+    mt = ANGLE(m);
     
-    % T = tic; %fprintf('\tGenerating Screen');
+    [mk   ] = 1/((SPL^2)/2)^0.5;
     
-    [mk   ] = 1/((SPL^2)/2)^0.5;%1/SPL; %PPI/LPI/SPL; %1/SPL; % 1/mp;
-    
-    % Elapsed time is 13.698273 seconds.
-    [mrx  ] = interp1(1:mvw,1:PPS:mvw, 'nearest'); %1:size(mv,2);
-    [mry  ] = interp1(1:mvh,1:PPS:mvh, 'nearest'); %1:size(mv,1);
+    [mrx  ] = interp1(1:mvw,1:PPS:mvw, 'nearest');
+    [mry  ] = interp1(1:mvh,1:PPS:mvh, 'nearest');
         
     %fprintf('.');
     
@@ -227,30 +393,21 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
         end
       else
         mq2 = ( im2double(  fast_rotate(im2uint8((mq/2)+0.5), -mt)  ) -0.5)*2.0;
-        %mq2 = im2double(fast_rotate(im2uint8(mq), -mt)); %imrotate(mq, -mt);
       end
     else
       mq2 = mq;
     end
-    
-    % fprintf('\n%s - %s - %d - %d\n', 'mq', class(mq), min(mq(:)), max(mq(:)));
-    % fprintf('\n%s - %s - %d - %d\n', 'mq2', class(mq2), min(mq2(:)), max(mq2(:)));
     
     xcrop   = floor((size(mq2,2) - numel(mrx))/2 + [1:numel(mrx)]);
     ycrop   = floor((size(mq2,1) - numel(mry))/2 + [1:numel(mry)]);
     mq2  = mq2(ycrop, xcrop);
     mq = mq2;
     
-    %fprintf(' ');
-        
-    % try toc(T); end
     
     %% Apply Screen
-    
-    % T = tic; %fprintf('\tApplying Screen... ');
         
-    [mvs  ] = mv; %imresize(mv, mp, 'nearest');
-    [mqs  ] = mq; %imresize(mq, mp, 'nearest');
+    [mvs  ] = mv;
+    [mqs  ] = mq;
     
     if DP > 0
       mvs(mvs>0) = mvs(mvs>0) + DP;
@@ -258,7 +415,7 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
     
     [mz   ] = mqs>((mvs(mry, mrx)-50).*0.02);
     
-    mzf = 1; % Floor & Ceil outside 1%-99%
+    mzf = 1;
     mz(mvs(mry, mrx)>100-mzf) = 0;
     mz(mvs(mry, mrx)<mzf)     = mzf;
     
@@ -277,82 +434,32 @@ function [ Output ] = grasppeScreen3( imagePath, ppi, spi, lpi, angle, printing,
       mzdbl(fmask)  = mzdbl(fmask)*(1-bp) + mblur(fmask)*bp;
     end
     
-    % try toc(T); end
-    
-    %%% Display Output
-    
-    % T = tic; fprintf('\tDisplaying Output... ');
-    %
-    % S = warning('off', 'images:imshow:magnificationMustBeFitForDockedFigure');
-    %
-    % imshow(mzdbl); %imresize(im2double(mz), 1));
-    %
-    % warning(S);
-    %
-    % try toc(T); end
-    
+    if outputScreen, screen{m} = mq; end
+    if outputRaster, raster{m} = ~mz; end 
+        
+    halftone(:,:,m) = mzdbl;
+        
     %% Save Screen
     
-    Screen = mq;
-    
-    s = warning('off', 'all');
-    mkdir(outpath(''));
-    s = warning(s);
-    
-    while exist(outpath(filename),'file') > 0
-      fileno = fileno+1;
-      filename = [prefix int2str(fileno) suffix];
-    end
-    
-    % T = tic; %fprintf('\tSaving Separation Files... ');
-    if NC > 1
-      imwrite(~mz, outpath(filename), 'Resolution', SPI, 'Description', imdisc);
-    end
-    
-    if outputScreen
-      imwrite(mq, outpath([prefix int2str(fileno) '.scrn' suffix]), ...
-        'Resolution', SPI, 'Compression', 'lzw', 'Description', imdisc);
-    end
-    
-    % try toc(T); end
-    
-    % mz = mq>((mv-50)*0.02);
+    % s = warning('off', 'all');
+    % FS.mkDir(outpath(''));
+    % s = warning(s);
     %
-    % ((mv-50)*0.02)
-    % mq = abs((cos(mx).*(1/mp)).*(cos(my).*(1/mp)));
-    % mz = mq>((mv-50)*0.02); imshow(mx, [])
-    % mq = abs((cos(mx)*(1/mp)).*(cos(my)*(1/mp)));
-    % mz = mq>((mv-50)*0.02); imshow(mx, [])
-    % (cos(mx)*(1/mp)).*(cos(my)*(1/mp))
+    % while exist(outpath(filename),'file') > 0
+    %   fileno = fileno+1;
+    %   filename = [prefix int2str(fileno) suffix];
+    % end
     
-    %end
+    % if NC > 1
+    %   imwrite(~mz, outpath(filename), 'Resolution', SPI, 'Description', imdisc);
+    % end
     
-    %% Wrapper Script
-    cmykRaster(:,:,m) = mzdbl;
+    % if outputScreen
+    %   imwrite(mq, outpath([prefix int2str(fileno) '.scrn' suffix]), ...
+    %     'Resolution', SPI, 'Compression', 'lzw', 'Description', imdisc);
+    % end
+    
   end
   
-  if ~bufferPersist, clear screenGrid screenFrequency screens; end
-  
-  % T = tic; %fprintf('\tSaving Composite... ');
-  
-  Output = cmykRaster;
-  
-  imwrite(cmykRaster, outpath([prefix suffix]), ...
-    'Resolution', SPI, 'Compression', 'lzw', 'Description', imdisc);
-  
-  % try toc(T); end
-  
-  
-  if nargout > 0
-    try stack.Screen  = Screen;       end
-%     try stack.Image   = Image;        end
-%     try stack.Input   = Input;        end
-%     try stack.Raster  = Raster;       end
-    try stack.Output  = Output;       end
-%     try stack.Bitmap  = logicalData;  end
-%     try stack.Preview = imageData;    end
-  end
-  
-  
+  if ~bufferPersist, clear screenGrid screenFrequency screens; end  
 end
-
